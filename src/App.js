b4742ffbd569805d00def590acefa336c766a68a
Chansoo <devhan1207@gmail.com>
2023-10-20 23:33:21 -0400

    applied some css in Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
