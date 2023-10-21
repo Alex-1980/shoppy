88010cc4c2d8fc742395b93551ee058cd5e33932
Chansoo <devhan1207@gmail.com>
2023-10-20 23:00:53 -0400

    added favicon and router

---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AllProducts() {
+  return (
+    <div>
+      All Products
+    </div>
+  );
+}
+
---
+++ src/pages/MyCart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MyCart() {
+  return (
+    <div>
+      My Cart
+    </div>
+  );
+}
+
