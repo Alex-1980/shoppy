528e8e1b6199c22f2fe19542591b182ac94acf56
Chansoo <devhan1207@gmail.com>
2023-10-26 11:39:52 -0400

    added protected route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
-import { login, logout, onUserStateChange } from "../api/firebase";
 import User from "./User";
-import Button from './ui/Button';
+import Button from "./ui/Button";
+import { useAuthContext } from "./context/AuthContext";
 
 export default function Navbar() {
-  const [user, setUser] = useState();
-  useEffect(() => {
-    onUserStateChange((user) => {
-      console.log(user);
-      setUser(user);
-    });
-  }, []);
+  const { user, login, logout } = useAuthContext();
 
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
@@ -23,15 +17,15 @@ export default function Navbar() {
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
+        {user && <Link to="/carts">Carts</Link>}
         {user && user.isAdmin && (
           <Link to="/products/new" className="text-2xl">
             <BsFillPencilFill />
           </Link>
         )}
         {user && <User user={user} />}
-        {!user && <Button text={'login'} onClick={login} />}
-        {user && <Button text={'logout'} onClick={logout} />}
+        {!user && <Button text={"login"} onClick={login} />}
+        {user && <Button text={"logout"} onClick={logout} />}
       </nav>
     </header>
   );
