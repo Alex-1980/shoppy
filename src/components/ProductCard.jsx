3eadd131af66c0451408b42905396814c95ada82
Chansoo <devhan1207@gmail.com>
2023-11-07 09:36:32 -0500

    added product detail page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function ProductCard({product: {id, image, title, category, price}}) {
+export default function ProductCard({
+  product,
+  product: { id, image, title, category, price },
+}) {
+  const navigate = useNavigate();
+
   return (
-    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
-      <img className='w-full' src={image} alt={title} />
-      <div className='mt-2 px-2 text-lg flex justify-between items-center'>
-        <h3 className='truncate'>{title}</h3>
+    <li
+      onClick={() => {navigate(`/products/${id}`, {state: {product}})}}
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+    >
+      <img className="w-full" src={image} alt={title} />
+      <div className="mt-2 px-2 text-lg flex justify-between items-center">
+        <h3 className="truncate">{title}</h3>
         <p>{`$${price}`}</p>
       </div>
-      <p className='mb-2 px-2 text-gray-600'>{category}</p>
+      <p className="mb-2 px-2 text-gray-600">{category}</p>
     </li>
   );
 }
-
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Button from "../components/ui/Button";
 
 export default function ProductDetail() {
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
+  const handleClick = (e) => {};
+
   return (
-    <div>
-      Product Details
-    </div>
+    <>
+      <p className="mx-12 mt-4 text-gray-700">{category}</p>
+      <section className="flex flex-col md:flex-row p-4">
+        <img className="w-full px-4 basis-7/12" src={image} alt={title} />
+        <div className="w-full basis-5/12 flex flex-col p-4">
+          <h2 className="text-3xl font-bold py-2">{title}</h2>
+          <p className="text-2xl font-bold py-2 border-b border-gray-400">
+            ${price}
+          </p>
+          <p className="py-4 text-lg">{description}</p>
+          <div className="flex items-center">
+            <label className="text-brand font-bold" htmlFor="select">
+              Option:
+            </label>
+            <select
+              id="select"
+              className="p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none"
+              onChange={handleSelect}
+              value={selected}
+            >
+              {options &&
+                options.map((option, index) => (
+                  <option key={index}>{option}</option>
+                ))}
+            </select>
+          </div>
+          <Button text="Add to cart" onClick={handleClick} />
+        </div>
+      </section>
+    </>
   );
 }
-
